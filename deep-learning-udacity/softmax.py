61dd87d325da0aea7142ee7a906f8b357f14bbf7
delizald <d_elizalderios@hotmail.com>
2017-06-25 19:46:15 -0500

    reset

---
+++ deep-learning-udacity/softmax.py
@@ -0,0 +1,19 @@
+########Creating  a softmax function
+#sudo apt-get install python-matplotlib
+import numpy as np
+
+test_data=[2.0,1.0,0.5]
+
+def softmax(x):
+	return np.exp(x) / np.sum(np.exp(x),axis=0)
+
+print (softmax(test_data))
+
+import matplotlib.pyplot as plt
+
+x = np.arange(-2,6.0,0.1)
+scores = np.vstack([x,np.ones_like(x),0.2 * np.ones_like(x)])
+
+plt.plot(x,softmax(scores).T, linewidth=2)
+plt.show()
+
